d2d26b518f12bc8554aa3ec70d048291a19baf9f
darwin <xu.dai.wen@126.com>
2025-09-06 13:22:25 +0800

    Update comments

--- device/ardu_test/ardu_test.ino
+++ device/ardu_test/ardu_test.ino
@@ -1,10 +1,14 @@
 void setup()
 {
     Serial.begin(115200);
+    // On NANO ESP32:
+    // The earliest number you can see this is about 70 to 80, so there is a
+    // window of about 770ms in which Serial.println(i) will lose data.
+
+    // On UNO:
+    // The earliest number you can see this is 0.
     for (int i = 0; i < 100; i++)
     {
-        // The earliest you can see this is about 77, so there is a window of
-        // about 770ms in which Serial.println(i) will lose data.
         Serial.println(i);
         delay(10);
     }
